f735c478c1ab186734493dc92aedaf9865d29f7f
AbdulRehman8153 <116027227+AbdulRehman8153@users.noreply.github.com>
2023-10-05 19:14:14 +0500

    Add New functions
    Add New Funcctions

---
+++ src/config.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$driveId="b!A1_K8Zkwa0ikSugI16DH_QsyIfJNq29CitDnlep5wSHMwdDaHNGUT4EBC7A1ches";
+
+
+$clientId="a1b259ca-22bc-4d80-99f5-a32b6a3cc40c";
+$clientSecret="7bn8Q~VOIR5eTr.3_YrTZdBFUKbUSV9h~H13Xb77";
+$tenantId="1a17fb93-b9e8-433d-9418-56455ea5573a";
+
+?>
